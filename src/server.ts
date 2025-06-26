6d0f1b4035baef53f8a7b71f0c1b04fb4e7baec9
Gabriel Ramalho <biel-pedro@hotmail.com>
2025-06-26 10:50:56 -0300

    feat: first commit

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { env } from './env'
+import { authenticateWithPassword } from './http/routes/auth/authenticate-with-password'
+import { createAccount } from './http/routes/auth/create-account'
+import { getProfile } from './http/routes/auth/get-profile'
+import { requestPasswordRecover } from './http/routes/auth/request-password-recover'
+import { resetPassword } from './http/routes/auth/reset-password'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Floki',
+      description: 'Organize your projects',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(fastifyCors)
+
+app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP running 🔥 at http://localhost:${env.PORT}`)
+})
